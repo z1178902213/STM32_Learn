b2f7f19444772d194d8e0c8907e9608cd0bf5070
WitcherCheung <1178902213@qq.com>
2026-01-06 22:58:53 +0800

    验证了位带操作的可行性，修复了宏定义的bug

--- Lesson5_Bitband/User/main.c
+++ Lesson5_Bitband/User/main.c
@@ -3,14 +3,14 @@
 
 #define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x02000000+((addr &0x000FFFFF)<<5)+(bitnum<<2))
 #define BIT_ADDR(addr, bitnum) (*(volatile unsigned long*)(BITBAND(addr, bitnum)))
-#define LED_RED BIT_ADDR(GPIOF->ODR, 6)
-#define LED_GREEN BIT_ADDR(GPIOF->ODR, 7)
-#define LED_BLUE BIT_ADDR(GPIOF->ODR, 8)
+#define LED_RED BIT_ADDR(GPIOF_BASE+0X14, 6)
+#define LED_GREEN BIT_ADDR(GPIOF_BASE+0X14, 7)
+#define LED_BLUE BIT_ADDR(GPIOF_BASE+0X14, 8)
 #define LED_ON 0
 #define LED_OFF 1
 
-void delay(unsigned int count){
-    for (;count>0;count--);
+void delay(__IO uint32_t count){
+    for(; count>0; count--);
 }
 
 int main(void){
