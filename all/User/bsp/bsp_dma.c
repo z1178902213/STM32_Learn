aa5f026ec7b6bd3cd064009dfb01e31fb46b6920
张伟杰 <1178902213@qq.com>
2026-02-10 16:52:44 +0800

    新建一个全部的项目都放在这

---
+++ all/User/bsp/bsp_dma.c
@@ -0,0 +1,9 @@
+#include "bsp_dma.h"
+
+
+void init_dma1(){
+	RCC_AHB1PeriphResetCmd(RCC_AHB1Periph_DMA1, ENABLE);
+	
+	
+	
+}
---
+++ all/User/bsp/include/bsp_dma.h
@@ -0,0 +1,8 @@
+#ifndef _BSP_DMA_H_
+#define _BSP_DMA_H_
+#include "stm32f4xx.h"
+
+
+
+#endif
+
